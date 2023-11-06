7c1bb9bd4bb213052db2c49e250cda16d74e3115
Yattmeo <matthewyyyeo11@gmail.com>
2023-11-06 21:30:10 +0800

    localstore for currentID

--- src/db/localstore.js
+++ src/db/localstore.js
@@ -10,6 +10,14 @@ const cart = reactive({
     inCart: []
 })
 
+export var currentID = reactive({
+    currentID: '12345',
+    updateCurrentID(new_id) {
+        this.currentID = new_id
+    }
+})
+
+
 export {
     cart,
     loggedInUser
