0334195a1b60a20726a43a666548aa94c422ba4b
GermaineGLS <germainegoh.2022@scis.smu.edu.sg>
2023-11-07 21:48:34 +0800

    .

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -42,7 +42,11 @@
                     </li>
     
                     <li>
+<<<<<<< Updated upstream
                         <RouterLink to="/" class="block py-2 pl-3 pr-4 text-white hover:bg-neutral-500 rounded md:bg-transparent  md:hover:text-[#50A060] md:p-0" aria-current="page">
+=======
+                        <RouterLink to="/" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-neutral-500 md:hover:bg-transparent  md:hover:text-[#50A060] md:p-0" aria-current="page">
+>>>>>>> Stashed changes
                             Home
                         </RouterLink>
                     </li>
@@ -57,14 +61,17 @@
                             Favourites
                         </RouterLink>
                     </li>
+<<<<<<< Updated upstream
                     <li>
                         <RouterLink to="/cart" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-neutral-500 md:hover:bg-transparent md:border-0 md:hover:text-[#50A060] md:p-0">
                             Listings
                         </RouterLink>
                     </li>
+=======
+>>>>>>> Stashed changes
     
                     <li>
-                        <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 pl-3 pr-4  text-white border-t border-gray-100 hover:bg-[#50A060] md:hover:bg-transparent md:border-0 md:hover:text-[#50A060] md:p-0 md:w-auto">
+                        <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 pl-3 pr-4  text-white border border-gray-100 hover:bg-[#50A060] md:hover:bg-transparent md:border-0 md:hover:text-[#50A060] md:p-0 md:w-auto">
                             <img src="src/assets/User Profile Icon.png" />
                             <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
