d58fca7630ca3081a803dfb0c223386db7bcf9d7
GermaineGLS <germainegoh.2022@scis.smu.edu.sg>
2023-11-08 21:18:22 +0800

    Update Checkout.vue

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,14 +1,27 @@
 <template>
-    <div class="cart-container">
-      <button @click="getcart" class="click-button">VIEW YOUR CART</button>
-      <br>
-      <br>
-  
-      <div class="cart-content">
-        <h1 class="cart-title">Your Cart</h1>
-        <br>
-        <ul id="cart" class="cart-list" style="color: black;"></ul>
+    <div class="cart">
+
+      <div class="display:block lg:flex">
+        <h1 class="text-gray-600 font-extrabold text-3xl text-center py-10 lg:ml-16">Your Cart</h1>
+
+        <div class="justify-center flex pb-8 lg:block lg:pt-8 lg:ml-16 ">
+          <button @click="getcart" 
+          class="items-center transition duration-500 ease-in-out bg-green-500 text-white sm:text-black sm:bg-white sm:border sm:border-black hover:border-white hover:bg-green-500 hover:text-white rounded-lg hover:scale-110 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-lg px-5 py-2.5 text-center">
+            View Your Cart
+          </button>
+        </div>
       </div>
+      
+      
+
+      <div class="cart-content">
+
+        <ul id="cart" class="" style="color: black;"></ul>
+      
+      </div>
+
+      
+  
     </div>
   </template>
   
@@ -58,72 +71,12 @@
   </script>
   
   <style scoped>
-    /* Add your CSS styles here */
-    .cart-container {
-      text-align: center;
-      margin: 0 auto;
-      max-width: 400px;
-      padding: 20px;
-      background-color: #f8f8f8;
-      border-radius: 10px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-      
-    }
   
-    .click-button {
-      background-color: rgb(156, 255, 200);
-      color: white;
-      padding: 10px 20px;
-      font-size: 16px;
-      border: none;
-      cursor: pointer;
-      border-radius: 5px;
-      color: black;
-    }
-
-    
-      
-    
-  
-    .click-button:hover {
-      background-color: rgb(156, 255, 200);
-    }
-  
-    .cart-title {
-      color: #333;
-      font-size: 24px;
-      margin-bottom: 20px;
-    }
   
     .cart-content {
       text-align: center;
     }
   
-    .cart-list {
-      list-style: none;
-      padding: 0;
-    }
-  
-    .cart-list img {
-      width: 50%;
-      display: block;
-      margin: 0 auto;
-    }
-  
-    .cart-list li {
-      color: #333;
-      font-size: 16px;
-      margin: 10px 0;
-    }
-  
-    .cart-list br {
-      display: block;
-      margin: 5px 0;
-    }
-  
-    .cart-total {
-      font-weight: bold;
-      font-size: 18px;
-    }
+    
   </style>
   
